69f4285f3393bc5f631beb09d12eb474aef6ce24
shachong <521045200@qq.com>
2023-04-18 15:09:51 +0800

    init ModuleB

---
+++ index.js
@@ -0,0 +1,10 @@
+import { Title, Body } from "./components";
+
+export const ModuleA = (props) => {
+  return (
+    <div>
+      <Title title="ModuleB Title" />
+      <Body content="ModuleB Body" />
+    </div>
+  );
+};
